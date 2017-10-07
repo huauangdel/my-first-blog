c8ad7c6f9825240c93f71ec56109336742df7839
whuang <huauangdel@gmail.com>
2017-10-06 22:16:15 -0500

    Cleanup

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    notUsed = 1
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
